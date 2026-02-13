d4d7ea49bd837d02cebbdfa2f6e2bc80f68baf48
C.M. Eshuis <cmeshuis@coma-113.vm.astro.rug.nl>
2026-02-13 12:57:51 +0100

    add new folders and files

---
+++ Task1/myfirst.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+x = 22/7
+
+
+# $$y=\frac{sin(x)}{x}$$
+
+# In[4]:
+
+
+pi = 22/7 # Processed on Jupyter Hub
+
+
+# ![BerneseMountainDog.jpg](attachment:2c778731-9416-4931-a224-d36612a5237a.jpg)
+
+# In[9]:
+
+
+y = 1
+
+y *= 6
+y *= 5
+y *= 4
+y *= 3
+y *= 2
+y *= 1
+print (y)
+
+
+# In[25]:
+
+
+h = 6.626*10**-34
+f = 2.42*10**28
+c = 3*10**8
+l = c/f
+E = h*(c/l)
+print (E)
+
+
+# In[26]:
+
+
+G = 6.67408*10**(-11)
+m_1 = 7.342*10**22
+m_2 = 5.9722*10**24
+r = 385000.6*10**3
+
+F = (G*m_1*m_2)/r**2
+print (F)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
