e25e4c3bd562f4da062bc214d5dbe18d49648dfe
C.M. Eshuis <cmeshuis@virgo05.intra.astro.rug.nl>
2026-03-05 12:33:53 +0100

    monte_carlo2

---
+++ Task4/Monte_carlo.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import numpy as np
+
+
+
+function = input("your function is: ")
+a = float(input("the lower bound is: "))
+b = float(input("the higher bound is: "))
+N = 100000   
+
+x = np.random.uniform(a,b,N)
+
+func = eval(function)
+
+#monte carlo formula
+((b-a)/N) * np.sum(func)
+
